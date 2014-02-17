60f67b8f9a2527a5ed8bc5b0e569350ffc2701b9
dcods22 <daniel.cody2@marist.edu>
2014-02-17 17:22:06 -0500

    Wrote helper functions

--- scala/encode.scala
+++ scala/encode.scala
@@ -1,6 +1,26 @@
 object encode {
+	
 	def main(args: Array[String]) {
 
+	}
+
+
+	def addCharInt(charInt, moveAmt) : Int = {
+		if(charInt < 64) 
+			return charInt;
+		else if((charInt + moveAmt) > 90)
+		    return (64 + ((charInt + moveAmt) - 90));
+		else
+		   return (charInt + moveAmt);
+	}
+
+	def subCharInt(charInt, moveAmt) : Int = {
+		if(charInt < 64)
+		    return charInt;
+		else if((charInt - moveAmt) < 64)
+			return (90 - (64 - (charInt - moveAmt)));
+		else
+		    return (charInt - moveAmt);
 	}
 
 	def encode(str : String, moveAmt : Int) : String = {
@@ -8,7 +28,7 @@ object encode {
 		val char : Char;
 		val rest : String;
 
-		if(str.size == 0) then{
+		if(str.size == 0){
 			'';
 		}
 		else{
