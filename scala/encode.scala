31be5ccc93a6f659820ea84b1da6a28272a22709
dcods22 <daniel.cody2@marist.edu>
2014-02-17 19:52:54 -0500

    Added Comments

--- scala/encode.scala
+++ scala/encode.scala
@@ -1,6 +1,8 @@
 object encode {
 	
+	//main method
 	def main(args: Array[String]) {
+		//test strings
 		val Estr : String = "THIS IS A TEST STRING FROM1122@";
 		val Dstr : String = "BPQA QA I BMAB ABZQVO NZWU1122@";
 		val Sstr : String = "HAL@";
@@ -11,30 +13,40 @@ object encode {
 		println(solve(Sstr, 26));
 	}
 
+	//method to figure out char int for encode
 	def addCharInt(charInt : Int, moveAmt : Int) : Int = {
+		//check if letter
 		if(charInt <= 64) 
 			return charInt;
+		//wrap around
 		else if((charInt + moveAmt) > 90)
 		    return (64 + ((charInt + moveAmt) - 90));
+		//no wrap
 		else
 		   return (charInt + moveAmt);
 	}
 
+	//method to figure out char int for decode
 	def subCharInt(charInt : Int, moveAmt : Int) : Int = {
+		//check if letter
 		if(charInt <= 64)
 		    return charInt;
+		//wrap around
 		else if((charInt - moveAmt) < 64)
 			return (90 - (64 - (charInt - moveAmt)));
+		//no wrap around
 		else
 		    return (charInt - moveAmt);
 	}
 
 	def encode(str : String, moveAmt : Int) : String = {
+		//vals for the char, rest of the string, char int and final char
 		val char : Char = str.charAt(0);
 		val rest : String = str.substring(1, str.size);
 		val charInt : Int = addCharInt(char.toInt, moveAmt);
 		val newChar : Char = charInt.toChar;
 
+		//check for recurseive call
 		if(rest.size == 0){
 			return newChar.toString;
 		}
@@ -45,11 +57,13 @@ object encode {
 
 
 	def decode(str : String, moveAmt : Int) : String = {
+		//vals for the char, rest of the string, char int and final char
 		val char : Char = str.charAt(0);
 		val rest : String = str.substring(1, str.size);
 		val charInt : Int = subCharInt(char.toInt, moveAmt);
 		val newChar : Char = charInt.toChar;
 
+		//check for recursive call
 		if(rest.size == 0){
 			return newChar.toString;
 		}
@@ -60,8 +74,10 @@ object encode {
 
 
 	def solve(str : String, solves : Int) : String = {
+		//check for recursive call
 		if (solves == -1) 
 			return "\n";
+		//call encode and recursive call
     	else 
     		"Ceaser " + solves.toString() + ": " + encode(str, solves) + " \n" + solve(str, solves - 1);
 	}
